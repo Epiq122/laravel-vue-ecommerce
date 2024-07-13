3a8f7ccb0ca0545093f2298ef300edd69149d3ea
Robert Gleason <l2ob122@gmail.com>
2024-07-13 13:34:21 -0700

    created the login component with styling, no logic

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {},
+    daisyui: {
+        themes: ["light", "dark", "cupcake", "forest"],
     },
-    plugins: [],
-}
+    plugins: [require("daisyui"), require("@tailwindcss/forms")],
+};
